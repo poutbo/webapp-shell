27b3105a4e2d49fed473ad5b9db73c3a85bf6605
Poutbo <poutbo@likejs.com>
2014-11-28 15:14:40 +0800

    init

--- client/js/start.js
+++ client/js/start.js
@@ -1,26 +1,32 @@
-var domain = 'http://doubi.io';
+// var domain = 'http://doubi.io';
+var domain = 'http://192.168.101.147';
+var path = '/github/webapp-shell';
 var bar = document.getElementById('bar');
-var status = document.getElementById('status');
+var text = document.getElementById('progress');
 var loading = document.getElementById('loading');
+
 window.addEventListener('message', function(e){
     if(e.origin !== domain) {
         return;
     }
     var json = JSON.parse(e.data);
     var progress = json.progress + '%';
+    bar.style.width = progress;
+    text.innerHTML = progress;
+    console.log(progress)
     switch (json.status) {
+        case 'progress':
+            loading.style.display = 'block';
+            break;
         case 'complete':
         case 'error':
-            top.location.replace(domain + '/web/app.html');
-            break;
-        case 'progress':
-            bar.style.width = progress;
-            status.innerHTML = progress;
-            loading.style.display = 'block';
+            setTimeout(function(){
+                top.location.replace(domain + path + '/web/app.html');
+            }, 500);
     }
 });
 
 var ifm = document.createElement('iframe');
-ifm.src = domain + '/web/cache.html';
+ifm.src = domain + path + '/web/cache.html';
 ifm.style.display = 'none';
 document.body.insertBefore(ifm, document.body.firstChild);
