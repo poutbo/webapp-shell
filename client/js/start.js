58106dca0bf82eadcaa8e3aee52f98bb795c8b67
Poutbo <poutbo@likejs.com>
2014-11-28 13:54:19 +0800

    init

--- client/js/start.js
+++ client/js/start.js
@@ -1,17 +1,22 @@
 var domain = 'http://doubi.io';
-
+var bar = document.getElementById('bar');
+var status = document.getElementById('status');
+var loading = document.getElementById('loading');
 window.addEventListener('message', function(e){
     if(e.origin !== domain) {
         return;
     }
-    var status = {complete:1, error:1};
     var json = JSON.parse(e.data);
     var progress = json.progress + '%';
-    document.getElementById('bar').style.width = progress;
-    document.getElementById('status').innerHTML = progress;
-    
-    if(status[json.status]){
-        top.location.replace(domain + '/web/app.html');
+    switch (json.status) {
+        case 'complete':
+        case 'error':
+            top.location.replace(domain + '/web/app.html');
+            break;
+        case 'progress':
+            bar.style.width = progress;
+            status.innerHTML = progress;
+            loading.style.display = 'block';
     }
 });
 
